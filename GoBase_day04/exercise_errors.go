a53c3b25cb1a374e038a89b7dee8ab79ee1ebaa2
liuyang-work <liuyang__work@163.com>
2020-11-06 22:46:04 +0800

    add

--- GoBase_day04/exercise_errors.go
+++ GoBase_day04/exercise_errors.go
@@ -4,8 +4,31 @@
 *Creat by LiuYang
 *Date 2020/11/1 14:00
  */
-package main
+ package main
 
+ import "fmt"
+ import "math"
+ 
+ type ErrNegativeSqrt float64
+
+ func (err ErrNegativeSqrt) Error() string {
+		res := fmt.Sprintf("cannot Sqrt negative number:%v",float64(err))
+		return res
+ }
+
+ func Sqrt(x float64) (float64,error) {
+	 if x < 0 {
+		return 0,ErrNegativeSqrt(x)
+	 }
+	 return math.Sqrt(x),nil
+	
+}
+
+ func main() {
+	 fmt.Println(Sqrt(-3))
+	 fmt.Println(Sqrt(-2))
+ }
+ 
 /*
 从之前的练习中复制 Sqrt 函数，修改它使其返回 error 值。
 
@@ -26,6 +49,5 @@ func (e ErrNegativeSqrt) Error() string
 可以通过先转换 e 来避免这个问题：fmt.Sprint(float64(e))。这是为什么呢？
 
 修改 Sqrt 函数，使其接受一个负数时，返回 ErrNegativeSqrt 值。
-
 
 */
--- GoBase_day04/exercise_stringer.go
+++ GoBase_day04/exercise_stringer.go
@@ -4,8 +4,28 @@
 *Creat by LiuYang
 *Date 2020/11/1 13:43
  */
-package main
+ package main
 
+ import "fmt"
+ 
+ type IPAddr [4]byte
+
+ func (ip IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
+ }
+ 
+ // TODO: 给 IPAddr 添加一个 "String() string" 方法
+ 
+ func main() {
+	 hosts := map[string]IPAddr{
+		 "loopback":  {127, 0, 0, 1},
+		 "googleDNS": {8, 8, 8, 8},
+	 }
+	 for name, ip := range hosts {
+		 fmt.Printf("%v: %v\n", name, ip)
+	 }
+ }
+ 
 /*
 通过让 IPAddr 类型实现 fmt.Stringer 来打印点号分隔的地址。
 
